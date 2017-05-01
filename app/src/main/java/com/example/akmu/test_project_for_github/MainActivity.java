2750d9cbca261a1e88f269a8fd69bdd6174b2cfb
harishkumar123 <harish.kumar.btech888@gmail.com>
2017-05-01 17:03:08 +0530

    on click listener on button 1 and button 2 in Mainactivity.java and name button in from send to send1 of first button in activity.xml

--- app/src/main/java/com/example/akmu/test_project_for_github/MainActivity.java
+++ app/src/main/java/com/example/akmu/test_project_for_github/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.akmu.test_project_for_github;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button send1,send2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        send1=(Button)findViewById(R.id.button);
+        send2=(Button)findViewById(R.id.button2);
+
+        send1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this,"this is send1 ",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        send2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this,"this is send 2 ",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
